688fcf22ccce0f42fb10d8582e3d2c4b9b7bf93d
Alexandre Cardoso <alexandre@Admins-Mac-mini.local>
2026-06-08 08:24:24 -0300

    primeiro Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Fragment } from "react/jsx-runtime";
+import { Heading } from "./components/Heading";
+
+import "./styles/theme.css";
+import "./styles/global.css";
+
+
 
 export function App() {
     return (
         <Fragment>
-            <h1>Hello World</h1>
+            <Heading/>
             <p>testando</p>
         </Fragment>
     )
