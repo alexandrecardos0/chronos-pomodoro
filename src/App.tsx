343c96566bcdd086b96c0391ca0e02c79cd69ed3
Alexandre Cardoso <alexandre@Admins-Mac-mini.local>
2026-06-08 09:31:06 -0300

    lucide react + heading.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Fragment } from "react/jsx-runtime";
 import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react";
+
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -9,7 +11,12 @@ import "./styles/global.css";
 export function App() {
     return (
         <Fragment>
-            <Heading/>
+            <Heading>
+                Hello World!
+                <button>
+                    <TimerIcon/>
+                </button>
+            </Heading>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 Lorem Ipsum has been the industry's standard dummy text ever since 1966, 
                 when designers at Letraset and James Mosley, the librarian at St Bride 
