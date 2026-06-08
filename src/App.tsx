2c965537b03bb91ebdfd35913ea50554ae8f5038
Alexandre Cardoso <alexandre@Admins-Mac-mini.local>
2026-06-08 17:17:18 -0300

    Logo componente + css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Fragment } from "react/jsx-runtime";
 import { Container } from "./components/Container/index.tsx";
 import { Heading } from "./components/Heading/index.tsx";
+import { Logo } from "./components/Logo/index.tsx";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+
 
 export function App() {
     return (
         <Fragment>
             <Container> 
-                <Heading>Logo</Heading>
+                <Logo/>
             </Container>
 
             <Container> 
