a2cd5a29718ec14ae42e18abdd40dabb04f59da1
Alexandre Cardoso <alexandre@Admins-Mac-mini.local>
2026-06-09 15:54:10 -0300

    estrutura geral definida

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Container } from "./components/Container/index.tsx";
 /*import { Heading } from "./components/Heading/index.tsx";*/
 import { Logo } from "./components/Logo/index.tsx";
 import { Menu } from "./components/Menu/index.tsx"
+import { CountDown } from "./components/CountDown/index.tsx";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+
 
 
 export function App() {
@@ -15,9 +17,37 @@ export function App() {
                 <Logo/>
             </Container>
 
-            <Container> 
-                <Menu/>
-            </Container>
+                <Container> 
+                    <Menu/>
+                </Container>
+
+                    <Container> 
+                        <CountDown/>
+                    </Container>
+
+                <Container>
+                    <form className="form" action="">
+                        <div className="formrow">
+                            <label htmlFor="meuInput">task</label>
+                            <input id="meuInput" type="text" />
+                        </div>
+
+                        <div className="formrow">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
+
+                        <div className="formrow">
+                            <p>Ciclos</p>
+                            <p>0000</p>
+                        </div>
+
+                        <div className="formrow">
+                            <button>Enviar</button>
+                        </div>
+
+
+                    </form>
+                </Container>    
 
         </Fragment>
     )
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,20 +6,20 @@ export function Menu() {
     <nav className={styles.menu}>
         <a className={styles.menuLink} href="#">
             <HouseIcon/>
-
         </a>
+
         <a className={styles.menuLink} href="#">
             <HistoryIcon/>
-
         </a>
+
         <a className={styles.menuLink} href="#">
             <SettingsIcon/>
-
         </a>
+
         <a className={styles.menuLink} href="#">
             <SunIcon/>
-
         </a>
+        
     </nav>
     );
 }
